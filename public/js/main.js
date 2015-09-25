fcbc6178edf1615106ad43e9c3fa17698023bc7a
Gary Siu <Gary@untypeable.com>
2015-09-25 01:28:22 +0100

    All done

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 // did I screw up linking the file?
 console.log('Hello world!');
-
+var endpoint = '/foods'
 // ready to roll!
 $(document).ready(function(){
 // declarations
@@ -9,21 +9,21 @@ $(document).ready(function(){
 // event listners
   setListeners();
 // first ajax request to index foods
-  $.ajax({
-    url: "/foods"
-  }).done(function(data){
+  $.get(endpoint)
+  .done(function(data){
     render(data);
   });
 })
 
 function setListeners(){
   $addFoodForm.on('submit', addFood);
+  $('body').on('click', '.delete', deleteFood);
 }
 
 function render(data){
   $foodList.empty();
   $.each(data, function(i, food){
-    $foodList.append('<div class="results"><a class="pure-button pure-button-error delete">X</a><span class="results-text">' + food.name + '</span><span class="pure-badge-info">' + food.yumminess +'</span></div>')
+    $foodList.append('<div class="results"><a class="pure-button pure-button-error delete" data-id="'+ food.id +'">X</a><span class="results-text">' + food.name + '</span><span class="pure-badge-info">' + food.yumminess +'</span></div>')
   })
 }
 
@@ -35,8 +35,19 @@ function addFood(){
   $('#name').val('');
   $('#yumminess').val('');
 
-  $.post('/foods/', newFood)
+  $.post(endpoint, newFood)
   .done(function(response){
-    $foodList.append('<div class="results"><a class="pure-button pure-button-error delete">X</a><span class="results-text">' + response.name + '</span><span class="pure-badge-info">' + response.yumminess +'</span></div>')
+    $foodList.append('<div class="results"><a class="pure-button pure-button-error delete" data-id="'+ response.id +'">X</a><span class="results-text">' + response.name + '</span><span class="pure-badge-info">' + response.yumminess +'</span></div>')
   });
+}
+
+function deleteFood(){
+  element = $(this);
+  $.ajax({
+    url: endpoint + '/' + element.data('id'),
+    method: 'delete'
+  })
+  .done(function(response){
+    element.parent().remove()
+  })
 }
